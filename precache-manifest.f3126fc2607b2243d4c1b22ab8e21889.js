852b980e8feb306f54aefee5a1a350a35e667eb3
Anurag1111111 <104418683+Anurag1111111@users.noreply.github.com>
2023-02-01 18:59:52 +0530

    Updates

---
+++ precache-manifest.f3126fc2607b2243d4c1b22ab8e21889.js
@@ -0,0 +1,458 @@
+self.__precacheManifest = [
+  {
+    "revision": "36ef9ac1b3daed8807ca",
+    "url": "/react-port-app/static/js/0.5ebd8f92.chunk.js"
+  },
+  {
+    "revision": "f3690b70eae6f28b5620",
+    "url": "/react-port-app/static/js/1.e561c4f7.chunk.js"
+  },
+  {
+    "revision": "a1e7689553e01cc495aa",
+    "url": "/react-port-app/static/js/2.788d8047.chunk.js"
+  },
+  {
+    "revision": "b10fda846500687cc75d",
+    "url": "/react-port-app/static/js/3.c66081fc.chunk.js"
+  },
+  {
+    "revision": "c76ae15c382acd8ca3da",
+    "url": "/react-port-app/static/js/4.6924f4df.chunk.js"
+  },
+  {
+    "revision": "589ebff7c7463aac4775",
+    "url": "/react-port-app/static/js/5.05cf0441.chunk.js"
+  },
+  {
+    "revision": "30a6c855021c126c7742",
+    "url": "/react-port-app/static/js/6.afd706bc.chunk.js"
+  },
+  {
+    "revision": "7a483fec61e05ec4dc4b",
+    "url": "/react-port-app/static/js/7.0b707cd4.chunk.js"
+  },
+  {
+    "revision": "6d3c0127ab78830e7309",
+    "url": "/react-port-app/static/js/8.2391c454.chunk.js"
+  },
+  {
+    "revision": "461bf9003300792f741e",
+    "url": "/react-port-app/static/js/9.76bfde8d.chunk.js"
+  },
+  {
+    "revision": "788db0174467e3112fe8",
+    "url": "/react-port-app/static/css/main.3f7c299a.chunk.css"
+  },
+  {
+    "revision": "788db0174467e3112fe8",
+    "url": "/react-port-app/static/js/main.377ddb4b.chunk.js"
+  },
+  {
+    "revision": "a935144c2cd7338aa0e5",
+    "url": "/react-port-app/static/js/runtime~main.ec1b95f3.js"
+  },
+  {
+    "revision": "8c0cdda6906ceb68f77c",
+    "url": "/react-port-app/static/css/12.752a395c.chunk.css"
+  },
+  {
+    "revision": "8c0cdda6906ceb68f77c",
+    "url": "/react-port-app/static/js/12.8446562e.chunk.js"
+  },
+  {
+    "revision": "4244fe2d753bfb179b16",
+    "url": "/react-port-app/static/js/13.8ab3f8ef.chunk.js"
+  },
+  {
+    "revision": "3baed64790f661fd7d67",
+    "url": "/react-port-app/static/js/14.ed5a77ab.chunk.js"
+  },
+  {
+    "revision": "035057279260e36280e7",
+    "url": "/react-port-app/static/js/15.332ece9e.chunk.js"
+  },
+  {
+    "revision": "6a6cda3f7824261c916f",
+    "url": "/react-port-app/static/js/16.c078782e.chunk.js"
+  },
+  {
+    "revision": "086e230dfdaea6191e08",
+    "url": "/react-port-app/static/js/17.7d63cf9c.chunk.js"
+  },
+  {
+    "revision": "5e4838393ebf70adfe61",
+    "url": "/react-port-app/static/js/18.e32652e3.chunk.js"
+  },
+  {
+    "revision": "3f0e2e6f10fe363a7690",
+    "url": "/react-port-app/static/js/19.27e5c0e0.chunk.js"
+  },
+  {
+    "revision": "e379666709d548fce5f6",
+    "url": "/react-port-app/static/js/20.1deecfac.chunk.js"
+  },
+  {
+    "revision": "df6e76f27fc2d015231a",
+    "url": "/react-port-app/static/js/21.fa38c48b.chunk.js"
+  },
+  {
+    "revision": "806258266a6a1727ce8b",
+    "url": "/react-port-app/static/js/22.25d8560f.chunk.js"
+  },
+  {
+    "revision": "f6f7740097cbeea98c44",
+    "url": "/react-port-app/static/js/23.b9d01655.chunk.js"
+  },
+  {
+    "revision": "b28a65fe0cc3c33e2b47",
+    "url": "/react-port-app/static/js/24.22eec79e.chunk.js"
+  },
+  {
+    "revision": "98020bb4851d87498b7e",
+    "url": "/react-port-app/static/js/25.ec036abf.chunk.js"
+  },
+  {
+    "revision": "e163a30f332a55c350ea",
+    "url": "/react-port-app/static/js/26.5e1aa387.chunk.js"
+  },
+  {
+    "revision": "72dd8f0d0fc9e168eb96",
+    "url": "/react-port-app/static/js/27.d2671ce6.chunk.js"
+  },
+  {
+    "revision": "915ac8f4e809ee370948",
+    "url": "/react-port-app/static/js/28.11b5f29e.chunk.js"
+  },
+  {
+    "revision": "22d78f79b9969f88dff3",
+    "url": "/react-port-app/static/js/29.cf6da48f.chunk.js"
+  },
+  {
+    "revision": "e9fdf505592b759d018f",
+    "url": "/react-port-app/static/js/30.2950927e.chunk.js"
+  },
+  {
+    "revision": "839899009485f31422ac",
+    "url": "/react-port-app/static/js/31.569cab32.chunk.js"
+  },
+  {
+    "revision": "d775125012d0dd365301",
+    "url": "/react-port-app/static/js/32.a9ab39c7.chunk.js"
+  },
+  {
+    "revision": "bf8d5dcbbb3bbe17fe9f",
+    "url": "/react-port-app/static/js/33.664947dd.chunk.js"
+  },
+  {
+    "revision": "32960044124e9f1803b6",
+    "url": "/react-port-app/static/js/34.f8c241e0.chunk.js"
+  },
+  {
+    "revision": "7ae1699bcd193bed9da8",
+    "url": "/react-port-app/static/js/35.c34c1b7a.chunk.js"
+  },
+  {
+    "revision": "d0997762a225e2db2f17",
+    "url": "/react-port-app/static/js/36.bb091c95.chunk.js"
+  },
+  {
+    "revision": "4dd334e057ab773e1b43",
+    "url": "/react-port-app/static/js/37.c9b8058c.chunk.js"
+  },
+  {
+    "revision": "40f044e4e3545bd8633d",
+    "url": "/react-port-app/static/js/38.beef1bb0.chunk.js"
+  },
+  {
+    "revision": "9a14cd0fc2263fbd2ca8",
+    "url": "/react-port-app/static/js/39.a0b0befa.chunk.js"
+  },
+  {
+    "revision": "bca207216112dd9636a1",
+    "url": "/react-port-app/static/js/40.af5be631.chunk.js"
+  },
+  {
+    "revision": "578b00ed0f0a91959aae",
+    "url": "/react-port-app/static/js/41.ba3a8838.chunk.js"
+  },
+  {
+    "revision": "095ce5fd079643d85eca",
+    "url": "/react-port-app/static/js/42.0e1a6eb2.chunk.js"
+  },
+  {
+    "revision": "bbdce11780fd5d9a9140",
+    "url": "/react-port-app/static/js/43.61a1282d.chunk.js"
+  },
+  {
+    "revision": "9940fba3ff21e7a759ed",
+    "url": "/react-port-app/static/js/44.28986c2d.chunk.js"
+  },
+  {
+    "revision": "cf4900b59fc237d62b48",
+    "url": "/react-port-app/static/js/45.0bf76ad1.chunk.js"
+  },
+  {
+    "revision": "c90ab1d6ad9a870feb0b",
+    "url": "/react-port-app/static/js/46.e6a1bc83.chunk.js"
+  },
+  {
+    "revision": "f50413bda05786d79c31",
+    "url": "/react-port-app/static/js/47.8354a71c.chunk.js"
+  },
+  {
+    "revision": "27781e159291519c75ea",
+    "url": "/react-port-app/static/js/48.65adc78c.chunk.js"
+  },
+  {
+    "revision": "ec7d9d302fa30b14ac46",
+    "url": "/react-port-app/static/js/49.7403d0f4.chunk.js"
+  },
+  {
+    "revision": "b973609ae1bc74c70c0f",
+    "url": "/react-port-app/static/js/50.f7e4444f.chunk.js"
+  },
+  {
+    "revision": "433a77ed22841f32fcb4",
+    "url": "/react-port-app/static/js/51.df6cb7c4.chunk.js"
+  },
+  {
+    "revision": "6356ece92d0b8973b1bc",
+    "url": "/react-port-app/static/js/52.451d52df.chunk.js"
+  },
+  {
+    "revision": "451685e7927bf6e0f8ff",
+    "url": "/react-port-app/static/js/53.93b66bf6.chunk.js"
+  },
+  {
+    "revision": "8cdcdd39bb81c44881ff",
+    "url": "/react-port-app/static/js/54.5ddd5878.chunk.js"
+  },
+  {
+    "revision": "c328cdff018b225aa736",
+    "url": "/react-port-app/static/js/55.bc58c43b.chunk.js"
+  },
+  {
+    "revision": "0416808f8cc3ed79d1a0",
+    "url": "/react-port-app/static/js/56.5b9d35ed.chunk.js"
+  },
+  {
+    "revision": "92f9bd29416ff67249ab",
+    "url": "/react-port-app/static/js/57.abd44f49.chunk.js"
+  },
+  {
+    "revision": "c05d0ca80e51500dead0",
+    "url": "/react-port-app/static/js/58.ce8f6661.chunk.js"
+  },
+  {
+    "revision": "e3235a66cf815fc69b96",
+    "url": "/react-port-app/static/js/59.2dd4fc2b.chunk.js"
+  },
+  {
+    "revision": "95fdcfc41535ae22b26d",
+    "url": "/react-port-app/static/js/60.3bd79b60.chunk.js"
+  },
+  {
+    "revision": "908ef145fe586dd5a002",
+    "url": "/react-port-app/static/js/61.d554b4d0.chunk.js"
+  },
+  {
+    "revision": "09fde664620dc27050fe",
+    "url": "/react-port-app/static/js/62.161f0672.chunk.js"
+  },
+  {
+    "revision": "6ed2d86da1e385793533",
+    "url": "/react-port-app/static/js/63.8f03413a.chunk.js"
+  },
+  {
+    "revision": "48be84d0e6df1cb682d8",
+    "url": "/react-port-app/static/js/64.6c1a3206.chunk.js"
+  },
+  {
+    "revision": "d44d9da39c1bef9c0d79",
+    "url": "/react-port-app/static/js/65.68ae2dbc.chunk.js"
+  },
+  {
+    "revision": "cadeecafd281a0c54258",
+    "url": "/react-port-app/static/js/66.b8c5798e.chunk.js"
+  },
+  {
+    "revision": "31835064329668c9b741",
+    "url": "/react-port-app/static/js/67.0be2b4c1.chunk.js"
+  },
+  {
+    "revision": "d71fb16b4ad69c658a40",
+    "url": "/react-port-app/static/js/68.fa202ac2.chunk.js"
+  },
+  {
+    "revision": "59de19dac3d3c0ea9e2c",
+    "url": "/react-port-app/static/js/69.f70a2dc4.chunk.js"
+  },
+  {
+    "revision": "91c933c8df033200e054",
+    "url": "/react-port-app/static/js/70.cf3abc47.chunk.js"
+  },
+  {
+    "revision": "058b213e2859db3211b4",
+    "url": "/react-port-app/static/js/71.45ca4632.chunk.js"
+  },
+  {
+    "revision": "af07065ea6109e4c3ef0",
+    "url": "/react-port-app/static/js/72.82986f26.chunk.js"
+  },
+  {
+    "revision": "00b6740ace1dddf6d14c",
+    "url": "/react-port-app/static/js/73.31f79545.chunk.js"
+  },
+  {
+    "revision": "22535fe52fe8d3ad9ace",
+    "url": "/react-port-app/static/js/74.533ef972.chunk.js"
+  },
+  {
+    "revision": "f244457ae0cb4a7140a2",
+    "url": "/react-port-app/static/js/75.c04388ac.chunk.js"
+  },
+  {
+    "revision": "1a6b6d4f2b136f2d0b49",
+    "url": "/react-port-app/static/js/76.3f754fc6.chunk.js"
+  },
+  {
+    "revision": "5d6feba976f529aa9ad0",
+    "url": "/react-port-app/static/js/77.126f05c5.chunk.js"
+  },
+  {
+    "revision": "d88581cef2b8a9abd008",
+    "url": "/react-port-app/static/js/78.5bb84552.chunk.js"
+  },
+  {
+    "revision": "8259f2a81f1c0c1d2e5a",
+    "url": "/react-port-app/static/js/79.b435530d.chunk.js"
+  },
+  {
+    "revision": "02b52091c7ff263bd850",
+    "url": "/react-port-app/static/js/80.2e5fa368.chunk.js"
+  },
+  {
+    "revision": "5a0d84637e09370a44c7",
+    "url": "/react-port-app/static/js/81.d1bc1cf6.chunk.js"
+  },
+  {
+    "revision": "24a87658f4dd4becc7bd",
+    "url": "/react-port-app/static/js/82.320ec209.chunk.js"
+  },
+  {
+    "revision": "de34f0abac3cd42b0789",
+    "url": "/react-port-app/static/js/83.955d0559.chunk.js"
+  },
+  {
+    "revision": "2be2f8e138c4c7cca86a",
+    "url": "/react-port-app/static/js/84.15d8e1f0.chunk.js"
+  },
+  {
+    "revision": "69c7cdccb41092da19fe",
+    "url": "/react-port-app/static/js/85.c5c86d3e.chunk.js"
+  },
+  {
+    "revision": "48d2ed8893b48b9e5fc4",
+    "url": "/react-port-app/static/js/86.efe071e8.chunk.js"
+  },
+  {
+    "revision": "8ebcc9d58a850b40e02e",
+    "url": "/react-port-app/static/js/87.d73f0041.chunk.js"
+  },
+  {
+    "revision": "7a254f64e150330108f2",
+    "url": "/react-port-app/static/js/88.44377c0d.chunk.js"
+  },
+  {
+    "revision": "578a603d9e3ff4083c07",
+    "url": "/react-port-app/static/js/89.dce4ba08.chunk.js"
+  },
+  {
+    "revision": "5637d13a9702e2d1879c",
+    "url": "/react-port-app/static/js/90.52219000.chunk.js"
+  },
+  {
+    "revision": "714e69bb44895fe9bb16",
+    "url": "/react-port-app/static/js/91.a06bbb77.chunk.js"
+  },
+  {
+    "revision": "a6a148508b0dbe794b1c",
+    "url": "/react-port-app/static/js/92.79a66502.chunk.js"
+  },
+  {
+    "revision": "4fbdba1f2c87afc1c454",
+    "url": "/react-port-app/static/js/93.c7a38125.chunk.js"
+  },
+  {
+    "revision": "c7b86e7d2196532d21c0",
+    "url": "/react-port-app/static/js/94.c34076b9.chunk.js"
+  },
+  {
+    "revision": "740c0c2c0eeb5944c240",
+    "url": "/react-port-app/static/js/95.7d79a3a2.chunk.js"
+  },
+  {
+    "revision": "54b5c1c65276ec2240f1",
+    "url": "/react-port-app/static/js/96.3923073c.chunk.js"
+  },
+  {
+    "revision": "1f8bb2966f6de8d5b7b1",
+    "url": "/react-port-app/static/js/97.5a4e5952.chunk.js"
+  },
+  {
+    "revision": "9befa4fd8eb510c72bfe",
+    "url": "/react-port-app/static/js/98.48335d1c.chunk.js"
+  },
+  {
+    "revision": "0151f02662949448f9ed",
+    "url": "/react-port-app/static/js/99.30bf4ee0.chunk.js"
+  },
+  {
+    "revision": "ccf785cace1549f81c83",
+    "url": "/react-port-app/static/js/100.7b8ed4e5.chunk.js"
+  },
+  {
+    "revision": "a27e94d88298784734b3",
+    "url": "/react-port-app/static/js/101.5c3bff36.chunk.js"
+  },
+  {
+    "revision": "30b306c45c4aa807abf7",
+    "url": "/react-port-app/static/js/102.0e09ca36.chunk.js"
+  },
+  {
+    "revision": "0ab6c52be6f798f104ea",
+    "url": "/react-port-app/static/js/103.7452d614.chunk.js"
+  },
+  {
+    "revision": "4bcf71eb74633a84f2d9",
+    "url": "/react-port-app/static/js/104.79b89178.chunk.js"
+  },
+  {
+    "revision": "f897947c0bb93616d8a8",
+    "url": "/react-port-app/static/js/105.8678ca2a.chunk.js"
+  },
+  {
+    "revision": "633e908771dea33d7d21",
+    "url": "/react-port-app/static/js/106.05124f3b.chunk.js"
+  },
+  {
+    "revision": "8a6ee89c52949e3ed159",
+    "url": "/react-port-app/static/js/107.1663bee0.chunk.js"
+  },
+  {
+    "revision": "b052a9630182273f429b",
+    "url": "/react-port-app/static/js/108.be41911c.chunk.js"
+  },
+  {
+    "revision": "c1ea9c4e59ba491b2eb5",
+    "url": "/react-port-app/static/js/109.c4257d9d.chunk.js"
+  },
+  {
+    "revision": "f889425eb50d773f8f46e5b47d77442e",
+    "url": "/react-port-app/static/media/anu.f889425e.jpg"
+  },
+  {
+    "revision": "7eb49b47be1fe7019232d284aa779030",
+    "url": "/react-port-app/index.html"
+  }
+];
